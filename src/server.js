bb36e84b239ca0e76f439667c4b625eb5dd4e2c7
“Almir” <almir_13@hotmail.com>
2026-02-23 22:17:33 -0300

    fix: aumentar limite JSON para 25mb (resolve foto nao salva)

--- src/server.js
+++ src/server.js
@@ -18,7 +18,8 @@ const app = express()
 
 app.use(helmet())
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '25mb' }))
+app.use(express.urlencoded({ extended: true, limit: '25mb' }))
 
 app.use('/api', authRoutes)
 app.use('/api', authPinRoutes)
